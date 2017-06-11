9c811abb0c1196e9cb13d1d5120df6923c6e7313
Ji Huang <cocafehj@qq.com>
2017-06-11 18:26:23 +0800

    gmp_helper: add api to headr
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- gmp_helper.h
+++ gmp_helper.h
@@ -8,4 +8,10 @@ enum {
         NUM_PRIME,
 };
 
+uint64_t urandom_read();
+void __mpz_urandomb(mpz_t rop, mp_bitcnt_t n);
+void __mpz_urandomm(mpz_t rop, const mpz_t n);
+int mpz_rand_bitlen(mpz_t rop, uint64_t len);
+int mpz_check_binlen(const mpz_t src, uint64_t len);
+
 #endif //SIMPLERSADIGEST_GMP_HELPER_H
