0fc1e7aba03ca33ee789b290d943f5251cd93ae9
Ji Huang <cocafehj@qq.com>
2017-06-11 13:48:28 +0800

    gmp_helper: add mpz_rand_bitlen()
    get randomly at given binary length
    
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- gmp_helper.c
+++ gmp_helper.c
@@ -27,3 +27,53 @@ uint64_t urandom_read()
 
         return res;
 }
+
+/**
+ * mpz_rand_bitlen() - get random number at given binary length
+ *
+ * @param   rop: result to store
+ * @param   len: binary length wanted
+ * @return  0 on success
+ */
+int mpz_rand_bitlen(mpz_t rop, uint64_t len)
+{
+        mpz_t res;
+        mpz_t rseed;
+        mpz_t upper;    /* not used */
+        mpz_t lower;
+        gmp_randstate_t rstate;
+
+        if (!rop)
+                return -EINVAL;
+
+        if (len < 1)
+                return -EINVAL;
+
+        mpz_inits(res, rseed, upper, lower, NULL);
+
+        mpz_set_ui(upper, 1);
+        mpz_set_ui(lower, 1);
+        mpz_set_ui(rseed, urandom_read());
+
+        mpz_mul_2exp(upper, upper, len);
+        mpz_mul_2exp(lower, lower, len - 1);
+
+        gmp_randinit_mt(rstate);
+        gmp_randseed(rstate, rseed);
+
+        while (1) {
+                /* return random number below (2^n) - 1*/
+                mpz_urandomb(res, rstate, len);
+
+                if (mpz_cmp(res, lower) >= 0) {
+                        break;
+                }
+        }
+
+        mpz_set(rop, res);
+
+        mpz_clears(res, rseed, upper, lower, NULL);
+        gmp_randclear(rstate);
+
+        return 0;
+}
