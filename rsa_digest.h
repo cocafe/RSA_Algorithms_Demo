616383cf45ac7e80a04ea619de02d384cb9c7e29
Ji Huang <cocafehj@qq.com>
2017-06-11 18:32:04 +0800

    project: adjust header files
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- rsa_digest.h
+++ rsa_digest.h
@@ -1,6 +1,14 @@
 #ifndef SIMPLERSADIGEST_RSA_DIGEST_H
 #define SIMPLERSADIGEST_RSA_DIGEST_H
 
+#ifndef __GMP_H__
+#error include gmp.h first
+#endif /* __GMP_H__ */
+
+#ifndef SIMPLERSADIGEST_GMP_HELPER_H
+#error include gmp_helper.h first
+#endif /* SIMPLERSADIGEST_GMP_HELPER_H */
+
 #define PRIMALITY_TEST_ACCURACY                 (5)
 
 enum {
