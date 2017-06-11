2991946e5070fad177ba9fb57d66a3eb112c4404
Ji Huang <cocafehj@qq.com>
2017-06-11 13:47:00 +0800

    gmp_helper: add urandom_read()
    use as seed generator, better than time()
    
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- gmp_helper.c
+++ gmp_helper.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <gmp.h>
+
 #include "gmp_helper.h"
+#include "rsa_digest.h"
+
+/**
+ * urandom_read() - read /dev/urandom
+ *
+ * @return  random uint64
+ */
+uint64_t urandom_read()
+{
+        FILE *dev;
+        uint64_t res;
+
+        dev = fopen("/dev/urandom", "r");
+        if (!dev)
+                return EIO;
+
+        fread(&res, sizeof(uint64_t), 1, dev);
+
+        fclose(dev);
+
+        return res;
+}
