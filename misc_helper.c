b8c50921249e053c330af202a493c90b504db2dc
Ji Huang <cocafehj@qq.com>
2017-06-14 20:14:22 +0800

    misc: fix memory_byte_dump()
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- misc_helper.c
+++ misc_helper.c
@@ -33,20 +33,21 @@ uint64_t urandom_read(void)
  * @param size: memory block in bytes
  * @param stream: file stream to dump to
  */
-void memory_byte_dump(uint8_t *blk, size_t size, FILE *stream)
+void memory_byte_dump(void *blk, size_t size, FILE *stream)
 {
 #define COUNT_RETURN_LINE               (4 * 8)
 #define COUNT_MAKE_SPACE                (4)
+        uint8_t *b;
         size_t i;
         int c;
 
-        for (i = 0, c = 1; i < size; ++i) {
-                fprintf(stream, "%02x", blk[i]);
+        for (i = 0, c = 1, b = blk; i < size; ++i) {
+                fprintf(stream, "%02x", b[i]);
 
                 if (!(c % COUNT_RETURN_LINE)) {
-                        fprintf(stdout, "\n");
+                        fprintf(stream, "\n");
                 } else if (!(c % COUNT_MAKE_SPACE)) {
-                        fprintf(stdout, " ");
+                        fprintf(stream, " ");
                 }
 
                 c++;
