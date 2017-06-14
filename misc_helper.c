4efe58895558bf63d268169b7566fd98abe18e81
Ji Huang <cocafehj@qq.com>
2017-06-14 19:52:30 +0800

    misc: add memory_byte_dump() helper
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- misc_helper.c
+++ misc_helper.c
@@ -25,3 +25,33 @@ uint64_t urandom_read(void)
 
         return res;
 }
+
+/**
+ * memory_byte_dump() - dump memory by bytes to file stream
+ *
+ * @param blk: pointer to memory block
+ * @param size: memory block in bytes
+ * @param stream: file stream to dump to
+ */
+void memory_byte_dump(uint8_t *blk, size_t size, FILE *stream)
+{
+#define COUNT_RETURN_LINE               (4 * 8)
+#define COUNT_MAKE_SPACE                (4)
+        size_t i;
+        int c;
+
+        for (i = 0, c = 1; i < size; ++i) {
+                fprintf(stream, "%02x", blk[i]);
+
+                if (!(c % COUNT_RETURN_LINE)) {
+                        fprintf(stdout, "\n");
+                } else if (!(c % COUNT_MAKE_SPACE)) {
+                        fprintf(stdout, " ");
+                }
+
+                c++;
+        }
+
+#undef COUNT_MAKE_SPACE
+#undef COUNT_RETURN_LINE
+}
--- misc_helper.h
+++ misc_helper.h
@@ -4,5 +4,6 @@
 #define ARRAY_SIZE(arr)                 (sizeof(arr) / sizeof((arr)[0]))
 
 uint64_t urandom_read(void);
+void memory_byte_dump(uint8_t *blk, size_t size, FILE *stream);
 
 #endif //SIMPLERSADIGEST_MISC_HELPER_H
