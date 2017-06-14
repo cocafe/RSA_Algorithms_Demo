595edb1059a5072e43565505ff42edb6ea238814
Ji Huang <cocafehj@qq.com>
2017-06-14 19:50:13 +0800

    misc: add ARRAY_SIZE() macro helper
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- misc_helper.h
+++ misc_helper.h
@@ -1,6 +1,8 @@
 #ifndef SIMPLERSADIGEST_MISC_HELPER_H
 #define SIMPLERSADIGEST_MISC_HELPER_H
 
+#define ARRAY_SIZE(arr)                 (sizeof(arr) / sizeof((arr)[0]))
+
 uint64_t urandom_read(void);
 
 #endif //SIMPLERSADIGEST_MISC_HELPER_H
