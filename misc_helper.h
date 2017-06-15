2c7e8eb2920f25b1c228afe639bcad51babdbdf1
Ji Huang <cocafehj@qq.com>
2017-06-15 19:27:09 +0800

    misc_helper: fix compliation error
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- misc_helper.h
+++ misc_helper.h
@@ -20,6 +20,8 @@
 #ifndef SIMPLERSADIGEST_MISC_HELPER_H
 #define SIMPLERSADIGEST_MISC_HELPER_H
 
+#include <stdio.h>
+
 #define ARRAY_SIZE(arr)                 (sizeof(arr) / sizeof((arr)[0]))
 
 uint64_t urandom_read(void);
