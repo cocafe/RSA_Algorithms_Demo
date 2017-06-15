3eecac441959ae50bd495b98d6a39d516bc8c7e1
Ji Huang <cocafehj@qq.com>
2017-06-15 19:06:20 +0800

    project: add license header
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- rsa.h
+++ rsa.h
@@ -1,3 +1,24 @@
+/**
+ * rsa.h - RSA public functions header
+ *
+ *      Author: cocafe <cocafehj@gmail.com> 2016
+ *
+ * Reference: <https://tools.ietf.org/html/rfc2313>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SIMPLERSADIGEST_RSA_DIGEST_H
 #define SIMPLERSADIGEST_RSA_DIGEST_H
 
